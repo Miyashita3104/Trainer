c18e6eba5eb043f87b85acf1a4f880e1c372e4c8
EC2 Default User <ec2-user@ip-172-31-40-80.us-west-2.compute.internal>
2018-10-20 10:28:47 +0000

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  routes :trainer_knowledges#index
   resources :trainer_knowledges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
